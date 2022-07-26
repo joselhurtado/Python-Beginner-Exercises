558e52f01ab61fd2f169191013385a1bd25c9535
Jose Hurtado <hello@hurtadojose.com>
2022-07-26 23:19:41 +0000

    Exercise 10.1 Compiled

--- .learn/resets/09-Random-Numbers/app.py
+++ .learn/resets/09-Random-Numbers/app.py
@@ -1,10 +1,10 @@
-#Code Already Compiled
-
 import random
 
 def get_randomInt():
   # CHANGE ONLY THIS LINE BELOW
-	random_number = random.randint(1, 10)
+	random_number = random.random()
 	return random_number
+
+
 
 print(get_randomInt())
